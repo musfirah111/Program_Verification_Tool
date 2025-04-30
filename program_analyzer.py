b4934e8362e1b741892dd44c74c99bef2c8e2c2d
Fatima-Rathore-2018 <rathore.fatima.27@gmail.com>
2025-04-30 20:41:21 +0500

    Update program_analyzer.py

---
+++ program_analyzer.py
@@ -0,0 +1,15 @@
+
+class ProgramVerifierAndEquivalenceChecker:
+    def __init__(self):
+        # To keep track of latest versions for each variable.
+        self.variable_versions = {}
+
+        # List to store ssa output.
+        self.ssa_lines = []
+
+        def process_individual_line(self, line):
+
+        def convert_into_ssa(self, code_lines):
+            for line in code_lines:
+                self.process_individual_line(line)
+
